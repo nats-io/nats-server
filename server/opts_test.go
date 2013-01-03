d0980376517b3e92d4767c4f6f92c872ce6494aa
Derek Collison <derek.collison@gmail.com>
2013-01-02 18:22:48 -0600

    Added additional defaults

--- server/opts_test.go
+++ server/opts_test.go
@@ -3,17 +3,22 @@
 package server
 
 import (
-	"testing"
 	"reflect"
+	"testing"
+	"time"
 )
 
 func TestDefaultOptions(t *testing.T) {
 	golden := &Options{
-		Host:         DEFAULT_HOST,
-		Port:         DEFAULT_PORT,
-		MaxConn:      DEFAULT_MAX_CONNECTIONS,
-		PingInterval: DEFAULT_PING_INTERVAL,
-		MaxPingsOut:  DEFAULT_PING_MAX_OUT,
+		Host:           DEFAULT_HOST,
+		Port:           DEFAULT_PORT,
+		MaxConn:        DEFAULT_MAX_CONNECTIONS,
+		PingInterval:   DEFAULT_PING_INTERVAL,
+		MaxPingsOut:    DEFAULT_PING_MAX_OUT,
+		SslTimeout:     float64(SSL_TIMEOUT) / float64(time.Second),
+		AuthTimeout:    float64(AUTH_TIMEOUT) / float64(time.Second),
+		MaxControlLine: MAX_CONTROL_LINE_SIZE,
+		MaxPayload:     MAX_PAYLOAD_SIZE,
 	}
 
 	opts := &Options{}
