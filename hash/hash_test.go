78802086193ad6789675a69178da30b52b44c13d
Derek Collison <derek.collison@gmail.com>
2013-06-03 09:50:06 -0700

    Cleanup on test subjects

--- hash/hash_test.go
+++ hash/hash_test.go
@@ -4,91 +4,90 @@ import (
 	"testing"
 )
 
-var foo = []byte("foo")
-var bar = []byte("bar")
-var baz = []byte("baz")
-var sub = []byte("apcera.continuum.router.foo.bar")
+// Representative of small and medium subjects.
+var smlKey = []byte("foo")
+var medKey = []byte("apcera.continuum.router.foo.bar")
 
 func Benchmark_Bernstein_SmallKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		Bernstein(foo)
+		Bernstein(smlKey)
 	}
 }
 
 func Benchmark_Murmur3___SmallKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		Murmur3(foo, M3Seed)
+		Murmur3(smlKey, M3Seed)
 	}
 }
 
 func Benchmark_FNV1A_____SmallKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		FNV1A(foo)
+		FNV1A(smlKey)
 	}
 }
 
 func Benchmark_Meiyan____SmallKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		Meiyan(foo)
+		Meiyan(smlKey)
 	}
 }
 
 func Benchmark_Jesteress_SmallKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		Jesteress(foo)
+		Jesteress(smlKey)
 	}
 }
 
 func Benchmark_Yorikke___SmallKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		Yorikke(foo)
+		Yorikke(smlKey)
 	}
 }
 
 func Benchmark_Bernstein___MedKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		Bernstein(sub)
+		Bernstein(medKey)
 	}
 }
 
 func Benchmark_Murmur3_____MedKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		Murmur3(sub, M3Seed)
+		Murmur3(medKey, M3Seed)
 	}
 }
 
 func Benchmark_FNV1A_______MedKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		FNV1A(sub)
+		FNV1A(medKey)
 	}
 }
 
 func Benchmark_Meiyan______MedKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		Meiyan(sub)
+		Meiyan(medKey)
 	}
 }
 
 func Benchmark_Jesteress___MedKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		Jesteress(sub)
+		Jesteress(medKey)
 	}
 }
 
 func Benchmark_Yorikke_____MedKey(b *testing.B) {
 	b.SetBytes(1)
 	for i := 0; i < b.N; i++ {
-		Yorikke(sub)
+		Yorikke(medKey)
 	}
 }
