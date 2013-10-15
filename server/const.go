2ebad0f1824cdee221984f6a7db7ea18bfbb2dc8
Derek Collison <derek@apcera.com>
2013-10-14 22:29:33 -0700

    Bumped version

--- server/const.go
+++ server/const.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "go-0.3.7"
+	VERSION = "go-0.3.8"
 
 	// DEFAULT_PORT is the deault port for client connections.
 	DEFAULT_PORT = 4222
