3e86a2cbeb1a5fbb001de14ab74ebf7c0e035532
Derek Collison <derek@apcera.com>
2014-07-09 09:45:08 -0700

    Added cluster restart test with queue subscribers

--- test/client_cluster_test.go
+++ test/client_cluster_test.go
@@ -1,10 +1,11 @@
-// Copyright 2013 Apcera Inc. All rights reserved.
+// Copyright 2013-2014 Apcera Inc. All rights reserved.
 
 package test
 
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -81,4 +82,149 @@ func TestServerRestartReSliceIssue(t *testing.T) {
 	case <-time.After(2 * time.Second):
 		t.Fatalf("Expected %d reconnects, got %d\n", numClients/2, reconnects)
 	}
+}
+
+// This will test queue subscriber semantics across a cluster in the presence
+// of server restarts.
+func TestServerRestartAndQueueSubs(t *testing.T) {
+	srvA, srvB, optsA, optsB := runServers(t)
+
+	urlA := fmt.Sprintf("nats://%s:%d/", optsA.Host, optsA.Port)
+	urlB := fmt.Sprintf("nats://%s:%d/", optsB.Host, optsB.Port)
+
+	// Client options
+	opts := nats.DefaultOptions
+	opts.Timeout = (5 * time.Second)
+	opts.ReconnectWait = (50 * time.Millisecond)
+	opts.MaxReconnect = 1000
+	opts.NoRandomize = true
+
+	// Allow us to block on a reconnect completion.
+	reconnectsDone := make(chan bool)
+	opts.ReconnectedCB = func(nc *nats.Conn) {
+		reconnectsDone <- true
+	}
+
+	// Helper to wait on a reconnect.
+	waitOnReconnect := func() {
+		var rcs int64
+		for {
+			select {
+			case <-reconnectsDone:
+				atomic.AddInt64(&rcs, 1)
+				if rcs >= 2 {
+					return
+				}
+			case <-time.After(2 * time.Second):
+				t.Fatalf("Expected a reconnect, timedout!\n")
+			}
+		}
+	}
+
+	// Create two clients..
+	opts.Servers = []string{urlA}
+	nc1, err := opts.Connect()
+	if err != nil {
+		t.Fatalf("Failed to create connection for nc1: %v\n", err)
+	}
+
+	opts.Servers = []string{urlB}
+	nc2, err := opts.Connect()
+	if err != nil {
+		t.Fatalf("Failed to create connection for nc2: %v\n", err)
+	}
+
+	c1, _ := nats.NewEncodedConn(nc1, "json")
+	c2, _ := nats.NewEncodedConn(nc2, "json")
+
+	// Flusher helper function.
+	flush := func() {
+		// Wait for processing.
+		c1.Flush()
+		c2.Flush()
+		// Wait for a short bit for cluster propogation.
+		time.Sleep(50 * time.Millisecond)
+	}
+
+	// To hold queue results.
+	results := make(map[int]int)
+	var mu sync.Mutex
+
+	// This corresponds to the subsriptions below.
+	const ExpectedMsgCount = 3
+
+	// Make sure we got what we needed, 1 msg only and all seqnos accounted for..
+	checkResults := func(numSent int) {
+		mu.Lock()
+		defer mu.Unlock()
+
+		for i := 0; i < numSent; i++ {
+			if results[i] != ExpectedMsgCount {
+				t.Fatalf("Received incorrect number of messages, [%d] for seq: %d\n", results[i], i)
+			}
+		}
+
+		// Auto reset results map
+		results = make(map[int]int)
+	}
+
+	subj := "foo.bar"
+	qgroup := "workers"
+
+	cb := func(seqno int) {
+		mu.Lock()
+		defer mu.Unlock()
+		results[seqno] = results[seqno] + 1
+	}
+
+	// Create queue subscribers
+	c1.QueueSubscribe(subj, qgroup, cb)
+	c2.QueueSubscribe(subj, qgroup, cb)
+
+	// Do a wildcard subscription.
+	c1.Subscribe("foo.*", cb)
+	c2.Subscribe("foo.*", cb)
+
+	// Wait for processing.
+	flush()
+
+	sendAndCheckMsgs := func(numToSend int) {
+		for i := 0; i < numToSend; i++ {
+			if i%2 == 0 {
+				c1.Publish(subj, i)
+			} else {
+				c2.Publish(subj, i)
+			}
+		}
+		// Wait for processing.
+		flush()
+		// Check Results
+		checkResults(numToSend)
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+	// Base Test
+	////////////////////////////////////////////////////////////////////////////
+
+	// Now send 10 messages, from each client..
+	sendAndCheckMsgs(10)
+
+	////////////////////////////////////////////////////////////////////////////
+	// Now restart SrvA and srvB, re-run test
+	////////////////////////////////////////////////////////////////////////////
+
+	srvA.Shutdown()
+	srvA = RunServer(optsA)
+	defer srvA.Shutdown()
+
+	srvB.Shutdown()
+	srvB = RunServer(optsB)
+	defer srvB.Shutdown()
+
+	waitOnReconnect()
+
+	time.Sleep(50 * time.Millisecond)
+
+	// Now send another 10 messages, from each client..
+	sendAndCheckMsgs(10)
 }
