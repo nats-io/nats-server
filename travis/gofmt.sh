dacf0e033add57129c55e4e2abe5b1e9facf7207
Ken Robertson <ken@apcera.com>
2014-08-12 23:01:21 -0700

    Fix Travis to properly fail the build on go fmt and go vet.
    When running go fmt/vet, they do not exit non-zero if they detect any issues,
    they only print out information. This wraps go fmt/vet in a script that
    captures the output to a temp file and checks if anything was written to it.
    If any was, it prints it out then exits 1.

---
+++ travis/gofmt.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# run go fmt and exit non-zero if it detected anything
+T=$(mktemp -t gofmt.XXXXX)
+go fmt ./... > $T
+if egrep -q '.*' "$T" ; then
+	echo "go fmt failed on the following files:"
+	cat "$T"
+	rm $T
+	exit 1
+fi
+rm $T
+exit 0
---
+++ travis/govet.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# run go vet and exit non-zero if it detected anything
+T=$(mktemp -t govet.XXXXX)
+go vet ./... > $T
+if egrep -q '.*' "$T" ; then
+	echo "go vet failed on the following files:"
+	cat "$T"
+	rm $T
+	exit 1
+fi
+rm $T
+exit 0
