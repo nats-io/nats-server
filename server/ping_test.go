e4fcd9de0aaa781abeb664b7b9935a82812b41cf
Derek Collison <derek@apcera.com>
2015-06-16 15:34:23 -0700

    Added server ping timer test

---
+++ server/ping_test.go
@@ -0,0 +1,33 @@
+// Copyright 2015 Apcera Inc. All rights reserved.
+
+package server
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/apcera/nats"
+)
+
+const PING_CLIENT_PORT = 11228
+
+var DefaultPingOptions = Options{
+	Host:         "localhost",
+	Port:         PING_CLIENT_PORT,
+	NoLog:        true,
+	NoSigs:       true,
+	PingInterval: 5 * time.Millisecond,
+}
+
+func TestPing(t *testing.T) {
+	s := RunServer(&DefaultPingOptions)
+	defer s.Shutdown()
+
+	nc, err := nats.Connect(fmt.Sprintf("nats://localhost:%d", PING_CLIENT_PORT))
+	if err != nil {
+		t.Fatalf("Error creating client: %v\n", err)
+	}
+	defer nc.Close()
+	time.Sleep(10 * time.Millisecond)
+}
