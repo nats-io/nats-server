f980ad072e6bf7fc61292856eb90473c4d5a194a
Kirill Pinchuk <k_pinchuk@wargaming.net>
2015-07-23 13:01:48 +0300

    Fix build via docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 docker build -t apcera/gnatsd_build .
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) -ti --name gnatsd_build apcera/gnatsd_build
-docker rm gnatsd_build
+docker run --rm=true -v `pwd`/gnatsd_build:/tmp/gnatsd_build \
+       -ti --name gnatsd_build apcera/gnatsd_build
+docker build -f Dockerfile.final -t apcera/gnatsd .
 docker rmi apcera/gnatsd_build
+rm -rf gnatsd_build/
