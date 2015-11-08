1145d9c731ecbd003a03c12a307805385cb59965
Derek Collison <derek.collison@gmail.com>
2015-11-08 14:40:29 -0800

    TLS beta

--- server/const.go
+++ server/const.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "0.6.8"
+	VERSION = "0.6.9.beta"
 
 	// DEFAULT_PORT is the deault port for client connections.
 	DEFAULT_PORT = 4222
