0675a8fd1fa274c52cc05cc0cc0bf4f1feaa5dd4
Colin Sullivan <colin.sullivan@apcera.com>
2015-11-19 16:22:57 -0700

    Update test to include expanded cipher suite.

--- server/opts_test.go
+++ server/opts_test.go
@@ -106,6 +106,7 @@ func TestTLSConfigFile(t *testing.T) {
 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 		//		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_RSA_WITH_RC4_128_SHA,
 	}
 	if !reflect.DeepEqual(tlsConfig.CipherSuites, ciphers) {
 		t.Fatalf("Got incorrect cipher suite list: [%+v]", tlsConfig.CipherSuites)
