7761ecd740882ca518f3b4ddd303c062618ecdd6
Derek Collison <derek.collison@gmail.com>
2016-04-04 19:01:18 -0700

    Added darwin, updated to Go1.6

--- scripts/cross_compile.sh
+++ scripts/cross_compile.sh
@@ -3,8 +3,8 @@ go get github.com/mitchellh/gox
 go get github.com/tcnksm/ghr
 
 export APPNAME="gnatsd"
-export OSARCH="linux/386 linux/amd64 linux/arm solaris/amd64 windows/386 windows/amd64"
-export DIRS="linux_386 linux_amd64 linux_arm solaris_amd64 windows_386 windows_amd64"
+export OSARCH="linux/386 linux/amd64 linux/arm darwin/amd64 solaris/amd64 windows/386 windows/amd64"
+export DIRS="linux_386 linux_amd64 linux_arm darwin_amd64 solaris_amd64 windows_386 windows_amd64"
 export OUTDIR="pkg"
 
 gox -osarch="$OSARCH" -output "$OUTDIR/$APPNAME-{{.OS}}_{{.Arch}}/$APPNAME"
@@ -14,4 +14,3 @@ for dir in $DIRS; do \
 	(cd $OUTDIR && zip -q $APPNAME-$dir.zip -r $APPNAME-$dir) ;\
 	echo "make $OUTDIR/$APPNAME-$dir.zip" ;\
 done
-
