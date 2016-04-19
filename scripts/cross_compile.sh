93e0b749f6af23e2f8bd17ba35a53df1b2b92eec
Derek Collison <derek.collison@gmail.com>
2016-04-18 19:25:58 -0700

    Allow tagged release names

--- scripts/cross_compile.sh
+++ scripts/cross_compile.sh
@@ -3,14 +3,19 @@ go get github.com/mitchellh/gox
 go get github.com/tcnksm/ghr
 
 export APPNAME="gnatsd"
-export OSARCH="linux/386 linux/amd64 linux/arm darwin/amd64 solaris/amd64 windows/386 windows/amd64"
-export DIRS="linux_386 linux_amd64 linux_arm darwin_amd64 solaris_amd64 windows_386 windows_amd64"
+export OSARCH="linux/386 linux/amd64 linux/arm darwin/amd64 windows/386 windows/amd64"
+export DIRS="linux-386 linux-amd64 linux-arm darwin-amd64 windows-386 windows-amd64"
 export OUTDIR="pkg"
 
-gox -osarch="$OSARCH" -output "$OUTDIR/$APPNAME-{{.OS}}_{{.Arch}}/$APPNAME"
+# If we have an arg, assume its a version tag and rename as appropriate.
+if [[ -n $1 ]]; then
+    export APPNAME=$APPNAME-$1
+fi
+
+gox -osarch="$OSARCH" -ldflags="-s -w" -output "$OUTDIR/$APPNAME-{{.OS}}-{{.Arch}}/$APPNAME"
 for dir in $DIRS; do \
-	(cp README.md $OUTDIR/$APPNAME-$dir/README.md) ;\
-	(cp LICENSE $OUTDIR/$APPNAME-$dir/LICENSE) ;\
-	(cd $OUTDIR && zip -q $APPNAME-$dir.zip -r $APPNAME-$dir) ;\
-	echo "make $OUTDIR/$APPNAME-$dir.zip" ;\
+    (cp README.md $OUTDIR/$APPNAME-$dir/README.md) ;\
+    (cp LICENSE $OUTDIR/$APPNAME-$dir/LICENSE) ;\
+    (cd $OUTDIR && zip -q $APPNAME-$dir.zip -r $APPNAME-$dir) ;\
+    echo "make $OUTDIR/$APPNAME-$dir.zip" ;\
 done
