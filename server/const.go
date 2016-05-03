7b33905db52c54bb288565cef0b88bcb3427bdc9
Derek Collison <derek.collison@gmail.com>
2016-05-03 13:00:18 -0700

    beta2

--- server/const.go
+++ server/const.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "0.8.0.beta"
+	VERSION = "0.8.0.beta2"
 
 	// DEFAULT_PORT is the deault port for client connections.
 	DEFAULT_PORT = 4222
