62923be357d99504f725ffc2ff27bcfc69f1943e
Ivan Kozlovic <ivan.kozlovic@apcera.com>
2016-08-08 14:56:28 -0600

    Bump version to 0.9.2

--- server/const.go
+++ server/const.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "0.9.0.beta"
+	VERSION = "0.9.2"
 
 	// DEFAULT_PORT is the default port for client connections.
 	DEFAULT_PORT = 4222
