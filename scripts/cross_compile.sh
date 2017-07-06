9d932b9c6afd5fe8ec27387ff31d9e1b3ccce26e
Ivan Kozlovic <ivan.kozlovic@apcera.com>
2017-07-06 17:07:58 -0600

    Replaced ARMv5 with ARMv6 in cross_compile
    Related to #527

--- scripts/cross_compile.sh
+++ scripts/cross_compile.sh
@@ -6,7 +6,7 @@ go get github.com/tcnksm/ghr
 
 export APPNAME="gnatsd"
 export OSARCH="linux/386 linux/amd64 darwin/amd64 windows/386 windows/amd64"
-export DIRS="linux-386 linux-amd64 linux-arm5 linux-arm7 darwin-amd64 windows-386 windows-amd64"
+export DIRS="linux-386 linux-amd64 linux-arm6 linux-arm7 darwin-amd64 windows-386 windows-amd64"
 export OUTDIR="pkg"
 
 # If we have an arg, assume its a version tag and rename as appropriate.
@@ -18,8 +18,8 @@ fi
 env CGO_ENABLED=0 gox -osarch="$OSARCH" -ldflags="-s -w" -output "$OUTDIR/$APPNAME-{{.OS}}-{{.Arch}}/gnatsd"
 
 # Be explicit about the ARM builds
-# ARMv5
-env CGO_ENABLED=0 GOARM=5 gox -osarch="linux/arm" -ldflags="-s -w" -output "$OUTDIR/$APPNAME-linux-arm5/gnatsd"
+# ARMv6
+env CGO_ENABLED=0 GOARM=6 gox -osarch="linux/arm" -ldflags="-s -w" -output "$OUTDIR/$APPNAME-linux-arm6/gnatsd"
 # ARMv7
 env CGO_ENABLED=0 GOARM=7 gox -osarch="linux/arm" -ldflags="-s -w" -output "$OUTDIR/$APPNAME-linux-arm7/gnatsd"
 
