f7f333ed2f2f7e3d54a9ebcd81ace7ee27788cfe
Gabriel Guzman <gabe@lifewaza.com>
2018-03-29 11:48:09 -0400

    Update

--- server/pse/pse_openbsd.go
+++ server/pse/pse_openbsd.go
@@ -10,6 +10,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Copied from pse_darwin.go
 
 package pse
 
