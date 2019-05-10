da9120345c83c2805abdcecfce54c8de174ea301
Derek Collison <derek@nats.io>
2019-05-09 18:13:31 -0700

    RC version bump [ci skip]
    Signed-off-by: Derek Collison <derek@nats.io>

--- server/const.go
+++ server/const.go
@@ -40,7 +40,7 @@ var (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.0.0-RC11"
+	VERSION = "2.0.0-RC12"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
