09954eee5c5a6bc5746607f01c1d8024861de279
Guangming Wang <guangming.wang@daocloud.io>
2019-08-15 22:12:57 +0800

    cleanup: fix word errors in errors.go
    Signed-off-by: Guangming Wang <guangming.wang@daocloud.io>

--- server/errors.go
+++ server/errors.go
@@ -40,14 +40,14 @@ var (
 	// ErrReservedPublishSubject represents an error condition when sending to a reserved subject, e.g. _SYS.>
 	ErrReservedPublishSubject = errors.New("reserved internal subject")
 
-	// ErrBadClientProtocol signals a client requested an invalud client protocol.
+	// ErrBadClientProtocol signals a client requested an invalid client protocol.
 	ErrBadClientProtocol = errors.New("invalid client protocol")
 
 	// ErrTooManyConnections signals a client that the maximum number of connections supported by the
 	// server has been reached.
 	ErrTooManyConnections = errors.New("maximum connections exceeded")
 
-	// ErrTooManyAccountConnections signals that an acount has reached its maximum number of active
+	// ErrTooManyAccountConnections signals that an account has reached its maximum number of active
 	// connections.
 	ErrTooManyAccountConnections = errors.New("maximum account active connections exceeded")
 
