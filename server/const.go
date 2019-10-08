d1e6c99d3976bde1dca9405db5f4e60d683cbfce
Derek Collison <derek@nats.io>
2019-10-08 14:50:23 -0700

    First pass pull mode, e.g. worker
    Signed-off-by: Derek Collison <derek@nats.io>

--- server/const.go
+++ server/const.go
@@ -40,7 +40,7 @@ var (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.1.6"
+	VERSION = "2.2.0-beta"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
