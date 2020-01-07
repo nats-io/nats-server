1f35f735f23d0a0d9175c5c2999d64f52fad91b5
R.I.Pienaar <rip@devco.net>
2020-01-07 15:18:25 +0100

    unexported import of Comma function from humanize

--- server/util.go
+++ server/util.go
@@ -16,6 +16,7 @@ package server
 import (
 	"errors"
 	"fmt"
+	"math"
 	"net"
 	"net/url"
 	"reflect"
@@ -109,4 +110,42 @@ func parseHostPort(hostPort string, defaultPort int) (host string, port int, err
 // false otherwise.
 func urlsAreEqual(u1, u2 *url.URL) bool {
 	return reflect.DeepEqual(u1, u2)
+}
+
+// comma produces a string form of the given number in base 10 with
+// commas after every three orders of magnitude.
+//
+// e.g. comma(834142) -> 834,142
+//
+// This function was copied from the github.com/dustin/go-humanize
+// package and is Copyright Dustin Sallings <dustin@spy.net>
+func comma(v int64) string {
+	sign := ""
+
+	// Min int64 can't be negated to a usable value, so it has to be special cased.
+	if v == math.MinInt64 {
+		return "-9,223,372,036,854,775,808"
+	}
+
+	if v < 0 {
+		sign = "-"
+		v = 0 - v
+	}
+
+	parts := []string{"", "", "", "", "", "", ""}
+	j := len(parts) - 1
+
+	for v > 999 {
+		parts[j] = strconv.FormatInt(v%1000, 10)
+		switch len(parts[j]) {
+		case 2:
+			parts[j] = "0" + parts[j]
+		case 1:
+			parts[j] = "00" + parts[j]
+		}
+		v = v / 1000
+		j--
+	}
+	parts[j] = strconv.Itoa(int(v))
+	return sign + strings.Join(parts[j:], ",")
 }
