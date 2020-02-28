6207c826ff7991da27c26c2468f3247a625a7332
R.I.Pienaar <rip@devco.net>
2020-02-28 14:25:53 -0800

    support jetstream client kind in a few places

--- server/client.go
+++ server/client.go
@@ -1298,6 +1298,8 @@ func (c *client) processErr(errStr string) {
 		c.Errorf("Leafnode Error %s", errStr)
 		c.leafProcessErr(errStr)
 		close = false
+	case JETSTREAM:
+		c.Errorf("JetStream Error %s", errStr)
 	}
 	if close {
 		c.closeConnection(ParseError)
@@ -3495,6 +3497,8 @@ func (c *client) typeString() string {
 		return "Gateway"
 	case LEAF:
 		return "LeafNode"
+	case JETSTREAM:
+		return "JetStream"
 	}
 	return "Unknown Type"
 }
