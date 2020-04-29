a5dbc20e94d46c3d77d9f44247d5345a1937940a
Derek Collison <derek@nats.io>
2020-04-28 19:33:57 -0700

    Fix for test failure
    Signed-off-by: Derek Collison <derek@nats.io>

--- server/gateway_test.go
+++ server/gateway_test.go
@@ -3902,7 +3902,9 @@ func ensureGWConnectTo(t *testing.T, s *Server, remoteGWName string, remoteGWSer
 				}
 			}
 			rg.Unlock()
-			nc.Close()
+			if nc != nil {
+				nc.Close()
+			}
 		} else {
 			good = true
 		}
