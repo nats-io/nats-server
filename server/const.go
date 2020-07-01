1004bd5caecb5391abef476c585f54f2aed18b40
Derek Collison <derek@nats.io>
2020-07-01 08:13:24 -0700

    version bump
    Signed-off-by: Derek Collison <derek@nats.io>

--- server/const.go
+++ server/const.go
@@ -40,7 +40,7 @@ var (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.2.0-beta.18"
+	VERSION = "2.2.0-beta.19"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
