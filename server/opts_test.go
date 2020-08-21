6bfa25376d13cbe5574382e70c246523200cb686
Matthias Hanel <mh@synadia.com>
2020-08-21 12:11:15 -0400

    Removing blank line from imports

--- server/opts_test.go
+++ server/opts_test.go
@@ -28,9 +28,8 @@ import (
 	"time"
 
 	"github.com/nats-io/jwt/v2"
-	"github.com/nats-io/nkeys"
-
 	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nkeys"
 )
 
 func checkOptionsEqual(t *testing.T, golden, opts *Options) {
