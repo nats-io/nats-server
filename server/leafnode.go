0d78bce9cfec416472bdada0d4c3db76947fc0ef
Ivan Kozlovic <ivan@synadia.com>
2021-01-15 12:00:34 -0700

    Fixed some leafnode issues introduced from JS cluster work
    Also fixed a flapper.
    
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- server/leafnode.go
+++ server/leafnode.go
@@ -993,7 +993,14 @@ func (c *client) processLeafnodeInfo(info *Info) error {
 		c.headers = supportsHeaders && info.Headers
 
 		// Remember the remote server.
-		c.leaf.remoteServer = info.Name
+		// Pre 2.2.0 servers are not sending their server name.
+		// In that case, use info.ID, which, for those servers, matches
+		// the content of the field `Name` in the leafnode CONNECT protocol.
+		if info.Name == _EMPTY_ {
+			c.leaf.remoteServer = info.ID
+		} else {
+			c.leaf.remoteServer = info.Name
+		}
 	}
 	// For both initial INFO and async INFO protocols, Possibly
 	// update our list of remote leafnode URLs we can connect to.
@@ -1098,7 +1105,7 @@ func (s *Server) setLeafNodeInfoHostPortAndIP() error {
 
 // Add the connection to the map of leaf nodes.
 // If `checkForDup` is true (invoked when a leafnode is accepted), then we check
-// if a connection already exists for the same server name (ID) and account.
+// if a connection already exists for the same server name and account.
 // That can happen when the remote is attempting to reconnect while the accepting
 // side did not detect the connection as broken yet.
 // But it can also happen when there is a misconfiguration and the remote is
@@ -1119,11 +1126,15 @@ func (s *Server) addLeafNodeConnection(c *client, srvName string, checkForDup bo
 
 	var old *client
 	s.mu.Lock()
-	if checkForDup {
+	// We check for empty because in some test we may send empty CONNECT{}
+	if checkForDup && srvName != _EMPTY_ {
 		for _, ol := range s.leafs {
 			ol.mu.Lock()
-			// We check for empty because in some test we may send empty CONNECT{}
-			if srvName != _EMPTY_ && ol.leaf.remoteServer == srvName && ol.acc.Name == accName {
+			// We care here only about non solicited Leafnode. This function
+			// is more about replacing stale connections than detecting loops.
+			// We have code for the loop detection elsewhere, which also delays
+			// attempt to reconnect.
+			if !ol.isSolicitedLeafNode() && ol.leaf.remoteServer == srvName && ol.acc.Name == accName {
 				old = ol
 			}
 			ol.mu.Unlock()
@@ -1218,7 +1229,15 @@ func (c *client) processLeafNodeConnect(s *Server, arg []byte, lang string) erro
 	c.headers = supportHeaders && proto.Headers
 
 	// Remember the remote server.
-	c.leaf.remoteServer = proto.Name
+	// We changed the leafnode CONNECT.Name json tag from "name" to "server_name",
+	// so for pre 2.2.0 servers, proto.Name (which is json "server_name") will be empty.
+	// However, CONNECT goes through common processing and the old json tag "name" is
+	// decoded into c.opts.Name (clientOpts struct), so use that if proto.Name is empty.
+	if proto.Name == _EMPTY_ {
+		c.leaf.remoteServer = c.opts.Name
+	} else {
+		c.leaf.remoteServer = proto.Name
+	}
 
 	// If the other side has declared itself a hub, so we will take on the spoke role.
 	if proto.Hub {
