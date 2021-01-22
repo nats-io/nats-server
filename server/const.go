393b4ca83dcfaa764a72335bd8bbbc6f7bfb77f1
Phil Pennock <philpennock@users.noreply.github.com>
2021-01-22 12:31:25 -0500

    Bump version's beta (#1834)

--- server/const.go
+++ server/const.go
@@ -40,7 +40,7 @@ var (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.2.0-beta.46"
+	VERSION = "2.2.0-beta.47"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
