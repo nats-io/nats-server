162bc1a0c4af33be03dba797295a7d5dac786d46
Derek Collison <derek@nats.io>
2021-01-25 11:54:48 -0800

    Bump beta version
    Signed-off-by: Derek Collison <derek@nats.io>

--- server/const.go
+++ server/const.go
@@ -40,7 +40,7 @@ var (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.2.0-beta.48"
+	VERSION = "2.2.0-beta.50"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
