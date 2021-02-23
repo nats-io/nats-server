0c561ed10b86625c207410b0fef22da19f118d67
Derek Collison <derek@nats.io>
2021-02-23 10:28:46 -0800

    Add in support for stream mirrors and sources. Add in proper support for stream updates in clustered mode. Don't send API updates without subjects, caused GW parser errors. Stream internal loops use their own clients now.
    Signed-off-by: Derek Collison <derek@nats.io>

--- server/jetstream.go
+++ server/jetstream.go
@@ -528,7 +528,9 @@ func (s *Server) getJetStream() *jetStream {
 func (a *Account) EnableJetStream(limits *JetStreamAccountLimits) error {
 	a.mu.RLock()
 	s := a.srv
+	accName := a.Name
 	a.mu.RUnlock()
+
 	if s == nil {
 		return fmt.Errorf("jetstream account not registered")
 	}
@@ -538,6 +540,11 @@ func (a *Account) EnableJetStream(limits *JetStreamAccountLimits) error {
 	}
 	if s.SystemAccount() == a {
 		return fmt.Errorf("jetstream can not be enabled on the system account")
+	}
+
+	// If we have gateways, switch to interest only mode.
+	if s.gateway.enabled {
+		s.switchAccountToInterestMode(accName)
 	}
 
 	// No limits means we dynamically set up limits.
