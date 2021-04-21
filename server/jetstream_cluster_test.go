5707c1132ce36ada224e41602d88029280cb7521
Derek Collison <derek@nats.io>
2021-04-20 17:08:47 -0700

    Move to dynamic ports
    Signed-off-by: Derek Collison <derek@nats.io>

--- server/jetstream_cluster_test.go
+++ server/jetstream_cluster_test.go
@@ -5923,8 +5923,8 @@ func createJetStreamSuperCluster(t *testing.T, numServersPer, numClusters int) *
 		t.Fatalf("Number of clusters must be > 1")
 	}
 
-	startClusterPorts := []int{5_022, 7_022, 10_022, 12_022, 16_332, 18_332, 40_332}
-	startGatewayPorts := []int{6_022, 8_022, 11_022, 17_332, 21_332, 42_332}
+	startClusterPorts := []int{50_022, 52_022, 55_022, 57_022, 59_332, 61_332}
+	startGatewayPorts := []int{50_122, 52_122, 55_122, 57_122, 59_432, 61_432}
 	startClusterPort := startClusterPorts[rand.Intn(len(startClusterPorts))]
 	startGWPort := startGatewayPorts[rand.Intn(len(startGatewayPorts))]
 
@@ -6192,7 +6192,7 @@ func createJetStreamClusterExplicit(t *testing.T, clusterName string, numServers
 
 func createJetStreamClusterWithTemplate(t *testing.T, tmpl string, clusterName string, numServers int) *cluster {
 	t.Helper()
-	startPorts := []int{9_022, 11_022, 14_022, 16_022, 20_332, 22_332, 33_332, 44_332}
+	startPorts := []int{49_222, 51_022, 54_022, 56_022, 60_332, 62_332}
 	port := startPorts[rand.Intn(len(startPorts))]
 	return createJetStreamCluster(t, tmpl, clusterName, _EMPTY_, numServers, port, true)
 }
