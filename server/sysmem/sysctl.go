f805f23d6e3940da14a85c108f34b7cb11d9a89f
Ivan Kozlovic <ivan@synadia.com>
2021-11-15 16:48:07 -0700

    Travis updates
    - Add Go 1.17
    - Fix go fmt from Go 1.17 (build directives)
    - Download version of misspell and staticcheck instead of doing
    "go get" since current staticcheck would be broken without go.mod
    
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- server/sysmem/sysctl.go
+++ server/sysmem/sysctl.go
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build darwin || freebsd || openbsd || dragonfly || netbsd
 // +build darwin freebsd openbsd dragonfly netbsd
 
 package sysmem
