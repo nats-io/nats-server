af776409915789215e06a3ec49798b86010ebf9c
Julius Žaromskis <julius@welink.com>
2022-06-06 11:09:46 +0300

    Update alpine image

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64/v8 -t juliuszaromskis/nats-server:latest -t juliuszaromskis/nats-server:2.7.0.1  -f ./docker/Dockerfile --push .
+docker buildx build --platform linux/amd64,linux/arm64/v8 -t juliuszaromskis/nats-server:latest -t juliuszaromskis/nats-server:2.7.0.2  -f ./docker/Dockerfile --push .
