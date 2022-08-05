815948f02fa70262c86a87ffadf38a6e1ba9c93e
Marco Primi <marco@mpri.me>
2022-08-05 09:00:22 -0700

    Exclude chaos tests helpers from default build

--- server/jetstream_chaos_helpers_test.go
+++ server/jetstream_chaos_helpers_test.go
@@ -11,6 +11,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build !skip_js_tests && !skip_js_cluster_tests && !skip_js_chaos_tests
+// +build !skip_js_tests,!skip_js_cluster_tests,!skip_js_chaos_tests
+
 package server
 
 import (
