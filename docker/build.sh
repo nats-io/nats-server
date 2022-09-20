4f44905e9bbadb72956262fb06c92033795f15af
Julius Žaromskis <julius@welink.com>
2022-09-20 16:32:13 +0300

    Update image tag

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64/v8 -t juliuszaromskis/nats-server:latest -t juliuszaromskis/nats-server:2.7.0.3  -f ./docker/Dockerfile --push .
+docker buildx build --platform linux/amd64,linux/arm64/v8 -t juliuszaromskis/nats-server:latest -t juliuszaromskis/nats-server:2.8.4.3  -f ./docker/Dockerfile --push .
