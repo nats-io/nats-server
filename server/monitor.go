abc08e8b0b27381faba2bc3a3af11066874241ce
Pouya Vedadiyan <vedadiyan@gmail.com>
2023-03-09 15:12:20 +0800

    Added subscription list to the connz API

--- server/monitor.go
+++ server/monitor.go
@@ -150,6 +150,7 @@ type ConnInfo struct {
 	NameTag        string         `json:"name_tag,omitempty"`
 	Tags           jwt.TagList    `json:"tags,omitempty"`
 	MQTTClient     string         `json:"mqtt_client,omitempty"` // This is the MQTT client id
+	Subz           []string
 }
 
 // TLSPeerCert contains basic information about a TLS peer certificate
@@ -542,6 +543,10 @@ func (s *Server) Connz(opts *ConnzOptions) (*Connz, error) {
 // Fills in the ConnInfo from the client.
 // client should be locked.
 func (ci *ConnInfo) fill(client *client, nc net.Conn, now time.Time, auth bool) {
+	subz := make([]string, 0)
+	for _, value := range client.subs {
+		subz = append(subz, string(value.subject))
+	}
 	ci.Cid = client.cid
 	ci.MQTTClient = client.getMQTTClientID()
 	ci.Kind = client.kindString()
@@ -562,7 +567,7 @@ func (ci *ConnInfo) fill(client *client, nc net.Conn, now time.Time, auth bool) 
 	// we need to use atomic here.
 	ci.InMsgs = atomic.LoadInt64(&client.inMsgs)
 	ci.InBytes = atomic.LoadInt64(&client.inBytes)
-
+	ci.Subz = subz
 	// If the connection is gone, too bad, we won't set TLSVersion and TLSCipher.
 	// Exclude clients that are still doing handshake so we don't block in
 	// ConnectionState().
