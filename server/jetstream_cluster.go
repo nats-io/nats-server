d02d59534f215d6c54db8ca8e32c89fbe7822147
Derek Collison <derek@nats.io>
2023-04-07 07:18:30 -0700

    Fix data race
    Signed-off-by: Derek Collison <derek@nats.io>

--- server/jetstream_cluster.go
+++ server/jetstream_cluster.go
@@ -7150,7 +7150,7 @@ func (mset *stream) processClusteredInboundMsg(subject, reply string, hdr, msg [
 	// Check to see if we are being overrun.
 	// TODO(dlc) - Make this a limit where we drop messages to protect ourselves, but allow to be configured.
 	if mset.clseq-(lseq+clfs) > streamLagWarnThreshold {
-		lerr := fmt.Errorf("JetStream stream '%s > %s' has high message lag", jsa.acc().Name, mset.cfg.Name)
+		lerr := fmt.Errorf("JetStream stream '%s > %s' has high message lag", jsa.acc().Name, name)
 		s.RateLimitWarnf(lerr.Error())
 	}
 	mset.clMu.Unlock()
