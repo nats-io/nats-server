cb9f8c0bf45906ccb9c4bd5c3979585751d58ecc
Derek Collison <derek@nats.io>
2023-07-30 11:53:18 -0700

    Fix to test for more efficient deleteBlocks
    Signed-off-by: Derek Collison <derek@nats.io>

--- server/norace_test.go
+++ server/norace_test.go
@@ -8730,7 +8730,7 @@ func TestNoRaceBinaryStreamSnapshotEncodingBasic(t *testing.T) {
 	require_True(t, ss.LastSeq == 3000)
 	require_True(t, ss.Msgs == 1000)
 	// We should have collapsed all these into 2 delete blocks.
-	require_True(t, len(ss.Deleted) == 2)
+	require_True(t, len(ss.Deleted) <= 2)
 	require_True(t, ss.Deleted.NumDeleted() == 2000)
 }
 
