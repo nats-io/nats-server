37268ecd4d1f7b97946626923e6c4b7637886930
Daniel Modler <modler@linkbit.io>
2023-08-11 13:48:41 +0200

    added unit test for cert_skip_invalid

--- test/configs/certs/tlsauth/certstore/import-p12-server.ps1
+++ test/configs/certs/tlsauth/certstore/import-p12-server.ps1
@@ -1,4 +1,7 @@
-$fileLocale = $PSScriptRoot + "\server.p12"
+$file=$args[0]
+if (!$file) { $file="server.p12 "}
+$fileLocale = $PSScriptRoot + "\" + $file
+echo "Installing certificate $fileLocale"
 $Pass = ConvertTo-SecureString -String 's3cr3t' -Force -AsPlainText
 $User = "whatever"
 $Cred = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User, $Pass
