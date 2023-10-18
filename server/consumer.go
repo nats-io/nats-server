b8842585453d9b8842ec6f45f245916c33525c1f
Neil Twigg <neil@nats.io>
2023-10-18 14:00:01 +0100

    Return no consumer found in error case, fix another potential panic
    Signed-off-by: Neil Twigg <neil@nats.io>

--- server/consumer.go
+++ server/consumer.go
@@ -2543,14 +2543,15 @@ func (o *consumer) infoWithSnapAndReply(snap bool, reply string) *ConsumerInfo {
 
 	// If we are replicated and we are not the leader we need to pull certain data from our store.
 	if rg != nil && rg.node != nil && !o.isLeader() && o.store != nil {
-		if state, err := o.store.BorrowState(); err == nil {
-			info.Delivered.Consumer, info.Delivered.Stream = state.Delivered.Consumer, state.Delivered.Stream
-			info.AckFloor.Consumer, info.AckFloor.Stream = state.AckFloor.Consumer, state.AckFloor.Stream
-			info.NumAckPending = len(state.Pending)
-			info.NumRedelivered = len(state.Redelivered)
-		} else {
-			o.srv.Warnf("Error retrieving consumer state from store: %s", err)
+		state, err := o.store.BorrowState()
+		if err != nil {
+			o.mu.Unlock()
+			return nil
 		}
+		info.Delivered.Consumer, info.Delivered.Stream = state.Delivered.Consumer, state.Delivered.Stream
+		info.AckFloor.Consumer, info.AckFloor.Stream = state.AckFloor.Consumer, state.AckFloor.Stream
+		info.NumAckPending = len(state.Pending)
+		info.NumRedelivered = len(state.Redelivered)
 	}
 
 	// Adjust active based on non-zero etc. Also make UTC here.
