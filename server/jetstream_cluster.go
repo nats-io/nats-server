548de33d95466b6f9748c882e9b309a8f9c3cc9b
Derek Collison <derek@nats.io>
2023-12-18 13:50:49 -0800

    Only call into update() iff the config has changed.
    Signed-off-by: Derek Collison <derek@nats.io>

--- server/jetstream_cluster.go
+++ server/jetstream_cluster.go
@@ -3604,20 +3604,23 @@ func (js *jetStream) processClusterCreateStream(acc *Account, sa *streamAssignme
 				}
 			}
 			mset.setStreamAssignment(sa)
-			if err = mset.updateWithAdvisory(sa.Config, false); err != nil {
-				s.Warnf("JetStream cluster error updating stream %q for account %q: %v", sa.Config.Name, acc.Name, err)
-				if osa != nil {
-					// Process the raft group and make sure it's running if needed.
-					js.createRaftGroup(acc.GetName(), osa.Group, storage, pprofLabels{
-						"type":    "stream",
-						"account": mset.accName(),
-						"stream":  mset.name(),
-					})
-					mset.setStreamAssignment(osa)
-				}
-				if rg.node != nil {
-					rg.node.Delete()
-					rg.node = nil
+			// Check if our config has really been updated.
+			if !reflect.DeepEqual(mset.config(), sa.Config) {
+				if err = mset.updateWithAdvisory(sa.Config, false); err != nil {
+					s.Warnf("JetStream cluster error updating stream %q for account %q: %v", sa.Config.Name, acc.Name, err)
+					if osa != nil {
+						// Process the raft group and make sure it's running if needed.
+						js.createRaftGroup(acc.GetName(), osa.Group, storage, pprofLabels{
+							"type":    "stream",
+							"account": mset.accName(),
+							"stream":  mset.name(),
+						})
+						mset.setStreamAssignment(osa)
+					}
+					if rg.node != nil {
+						rg.node.Delete()
+						rg.node = nil
+					}
 				}
 			}
 		} else if err == NewJSStreamNotFoundError() {
