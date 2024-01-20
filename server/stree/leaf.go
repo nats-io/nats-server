d9235abbe1ed341133959b6e523d84b1c524c060
Derek Collison <derek@nats.io>
2024-01-20 10:59:36 -0800

    [IMPROVED] NumPending calculations and subject index memory in filestore (#4960)
    Swapped out psim as a go hashmap for our stree implementation.
    
    Stree is an adaptive radix tree implementation used for storing and
    retrieving literal subjects. It also allows quick matching to wildcard
    subjects, which is it's major design goal along with using less memory
    in high subject cardinality situations.
    
    This will be used in the filestore implementation to replace the PSIM
    hash map which was fast at insert and lookup but suffered when trying to
    filter based on wildcard subjects.
    
    This is used specifically in calculations on NumPending with a wildcard,
    and given we push folks to use larger muxed streams with down filtered
    consumers and/or mirrors this was becoming a performance issue.
    
    Signed-off-by: Derek Collison <derek@nats.io>
    
    ---------
    
    Signed-off-by: Derek Collison <derek@nats.io>
    Signed-off-by: Neil Twigg <neil@nats.io>
    Co-authored-by: Neil Twigg <neil@nats.io>

---
+++ server/stree/leaf.go
@@ -0,0 +1,50 @@
+// Copyright 2023-2024 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package stree
+
+import (
+	"bytes"
+)
+
+// Leaf node
+type leaf[T any] struct {
+	// This could be the whole subject, but most likely just the suffix portion.
+	// We will only store the suffix here and assume all prior prefix paths have
+	// been checked once we arrive at this leafnode.
+	suffix []byte
+	value  T
+}
+
+func newLeaf[T any](suffix []byte, value T) *leaf[T] {
+	return &leaf[T]{copyBytes(suffix), value}
+}
+
+func (n *leaf[T]) isLeaf() bool                               { return true }
+func (n *leaf[T]) base() *meta                                { return nil }
+func (n *leaf[T]) match(subject []byte) bool                  { return bytes.Equal(subject, n.suffix) }
+func (n *leaf[T]) setSuffix(suffix []byte)                    { n.suffix = copyBytes(suffix) }
+func (n *leaf[T]) isFull() bool                               { return true }
+func (n *leaf[T]) matchParts(parts [][]byte) ([][]byte, bool) { return matchParts(parts, n.suffix) }
+func (n *leaf[T]) iter(f func(node) bool)                     {}
+func (n *leaf[T]) children() []node                           { return nil }
+func (n *leaf[T]) numChildren() uint16                        { return 0 }
+func (n *leaf[T]) path() []byte                               { return n.suffix }
+
+// Not applicable to leafs and should not be called, so panic if we do.
+func (n *leaf[T]) setPrefix(pre []byte)    { panic("setPrefix called on leaf") }
+func (n *leaf[T]) addChild(_ byte, _ node) { panic("addChild called on leaf") }
+func (n *leaf[T]) findChild(_ byte) *node  { panic("findChild called on leaf") }
+func (n *leaf[T]) grow() node              { panic("grow called on leaf") }
+func (n *leaf[T]) deleteChild(_ byte)      { panic("deleteChild called on leaf") }
+func (n *leaf[T]) shrink() node            { panic("shrink called on leaf") }
