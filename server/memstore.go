76fb7cc496b4cd583823429bba52c356438df95f
SLeonhar <stefan.leonhartsberger@frequentis.com>
2024-02-21 11:30:47 +0100

    Added simplistic perf test for Compact & fixed the issue to proof it The underlying Memory issue will persist. The test is relying on timing now and should be improved to be deterministic.

--- server/memstore.go
+++ server/memstore.go
@@ -792,10 +792,11 @@ func (ms *memStore) Compact(seq uint64) (uint64, error) {
 			ms.mu.Unlock()
 			return 0, ErrStoreMsgNotFound
 		}
+		oldFirstSeq := ms.state.FirstSeq - 1
 		ms.state.FirstSeq = seq
 		ms.state.FirstTime = time.Unix(0, sm.ts).UTC()
 
-		for seq := seq - 1; seq > 0; seq-- {
+		for seq := seq - 1; seq > oldFirstSeq; seq-- {
 			if sm := ms.msgs[seq]; sm != nil {
 				bytes += memStoreMsgSize(sm.subj, sm.hdr, sm.msg)
 				purged++
