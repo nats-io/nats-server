a26043ff84c4da69035cb776ff16ddd3a7dd4118
Byron Ruth <byron@nats.io>
2024-02-27 15:08:16 -0500

    Release 2.11.0-preview.1
    Signed-off-by: Byron Ruth <byron@nats.io>

--- server/const.go
+++ server/const.go
@@ -41,7 +41,7 @@ var (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.11.0-dev"
+	VERSION = "2.11.0-preview.1"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
