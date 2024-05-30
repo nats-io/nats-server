b2f9c4e5dc6c24ee2b144a802170aa783911bf34
Neil Twigg <neil@nats.io>
2024-05-30 15:46:05 +0100

    Lock-free IP queues
    Signed-off-by: Neil Twigg <neil@nats.io>

--- server/ipqueue_lockfree.go
+++ server/ipqueue_lockfree.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build lockfreeipq
+//go:build !lockfreeipq
 
 package server
 
@@ -22,17 +22,22 @@ import (
 
 const ipQueueDefaultMaxRecycleSize = 4 * 1024
 
-// This is a generic intra-process queue.
+// This is a lock-free implementation of a generic intra-process queue.
 type ipQueue[T any] struct {
-	inprogress int64
-	sync.Mutex
-	ch   chan struct{}
-	elts []T
-	pos  int
-	pool *sync.Pool
-	mrs  int
-	name string
-	m    *sync.Map
+	ch         chan struct{}                     // Used to signal waiting listeners
+	head       atomic.Pointer[ipQueueElement[T]] // First element in the queue
+	tail       atomic.Pointer[ipQueueElement[T]] // Last element in the queue
+	sz         atomic.Int64                      // Current size of the IP queue
+	inprogress atomic.Int64                      // How many elements have been pulled off?
+	pool       *sync.Pool                        // Elements are recycled here
+	mrs        int                               // Max recycle size
+	name       string                            // Name of this queue
+	m          *sync.Map                         // Map to all registered IP queues
+}
+
+type ipQueueElement[T any] struct {
+	v    atomic.Pointer[T]
+	next atomic.Pointer[ipQueueElement[T]]
 }
 
 type ipQueueOpts struct {
@@ -40,14 +45,6 @@ type ipQueueOpts struct {
 }
 
 type ipQueueOpt func(*ipQueueOpts)
-
-// This option allows to set the maximum recycle size when attempting
-// to put back a slice to the pool.
-func ipQueue_MaxRecycleSize(max int) ipQueueOpt {
-	return func(o *ipQueueOpts) {
-		o.maxRecycleSize = max
-	}
-}
 
 func newIPQueue[T any](s *Server, name string, opts ...ipQueueOpt) *ipQueue[T] {
 	qo := ipQueueOpts{maxRecycleSize: ipQueueDefaultMaxRecycleSize}
@@ -58,42 +55,37 @@ func newIPQueue[T any](s *Server, name string, opts ...ipQueueOpt) *ipQueue[T] {
 		ch:   make(chan struct{}, 1),
 		mrs:  qo.maxRecycleSize,
 		pool: &sync.Pool{},
-		name: name,
 		m:    &s.ipQueues,
 	}
 	s.ipQueues.Store(name, q)
 	return q
 }
 
+func (q *ipQueue[T]) Lock()   {} // Gets inlined away but satisfies callsites
+func (q *ipQueue[T]) Unlock() {} // Gets inlined away but satisfies callsites
+
 // Add the element `e` to the queue, notifying the queue channel's `ch` if the
 // entry is the first to be added, and returns the length of the queue after
 // this element is added.
 func (q *ipQueue[T]) push(e T) int {
-	var signal bool
-	q.Lock()
-	l := len(q.elts) - q.pos
-	if l == 0 {
-		signal = true
-		eltsi := q.pool.Get()
-		if eltsi != nil {
-			// Reason we use pointer to slice instead of slice is explained
-			// here: https://staticcheck.io/docs/checks#SA6002
-			q.elts = (*(eltsi.(*[]T)))[:0]
+	new := &ipQueueElement[T]{}
+	new.v.Store(&e)
+	for {
+		tail := q.tail.Load()
+		if !q.tail.CompareAndSwap(tail, new) {
+			continue
 		}
-		if cap(q.elts) == 0 {
-			q.elts = make([]T, 0, 32)
+		if tail != nil {
+			tail.next.CompareAndSwap(nil, new)
 		}
+		if q.head.CompareAndSwap(nil, new) {
+			select {
+			case q.ch <- struct{}{}:
+			default:
+			}
+		}
+		return int(q.sz.Add(1))
 	}
-	q.elts = append(q.elts, e)
-	l++
-	q.Unlock()
-	if signal {
-		select {
-		case q.ch <- struct{}{}:
-		default:
-		}
-	}
-	return l
 }
 
 // Returns the whole list of elements currently present in the queue,
@@ -109,22 +101,35 @@ func (q *ipQueue[T]) pop() []T {
 	if q == nil {
 		return nil
 	}
-	var elts []T
-	q.Lock()
-	if q.pos == 0 {
-		elts = q.elts
-	} else {
-		elts = q.elts[q.pos:]
+	for {
+		head := q.head.Load()
+		if head == nil {
+			return nil
+		}
+		tail := q.tail.Load()
+		if !q.head.CompareAndSwap(head, nil) {
+			continue
+		}
+		q.tail.CompareAndSwap(tail, nil)
+		var elts []T
+		if eltsi := q.pool.Get(); eltsi != nil {
+			// Reason we use pointer to slice instead of slice is explained
+			// here: https://staticcheck.io/docs/checks#SA6002
+			elts = (*(eltsi.(*[]T)))[:0]
+		}
+		if cap(elts) == 0 {
+			elts = make([]T, 0, 32)
+		}
+		for ptr := head; ptr != nil; ptr = ptr.next.Load() {
+			if v := ptr.v.Load(); v != nil {
+				elts = append(elts, *ptr.v.Load())
+			}
+		}
+		c := int64(len(elts))
+		q.inprogress.Add(c)
+		q.sz.Add(-c)
+		return elts
 	}
-	q.elts, q.pos = nil, 0
-	atomic.AddInt64(&q.inprogress, int64(len(elts)))
-	q.Unlock()
-	return elts
-}
-
-func (q *ipQueue[T]) resetAndReturnToPool(elts *[]T) {
-	(*elts) = (*elts)[:0]
-	q.pool.Put(elts)
 }
 
 // Returns the first element from the queue, if any. See comment above
@@ -133,29 +138,22 @@ func (q *ipQueue[T]) resetAndReturnToPool(elts *[]T) {
 // boolean return value to ensure that the value is genuine and not a
 // default empty value.
 func (q *ipQueue[T]) popOne() (T, bool) {
-	q.Lock()
-	l := len(q.elts) - q.pos
-	if l < 1 {
-		q.Unlock()
-		var empty T
-		return empty, false
-	}
-	e := q.elts[q.pos]
-	q.pos++
-	l--
-	if l > 0 {
-		// We need to re-signal
-		select {
-		case q.ch <- struct{}{}:
-		default:
+	var elt T
+	for {
+		head := q.head.Load()
+		if head == nil {
+			return elt, false
 		}
-	} else {
-		// We have just emptied the queue, so we can recycle now.
-		q.resetAndReturnToPool(&q.elts)
-		q.elts, q.pos = nil, 0
+		next := head.next.Load()
+		if q.head.CompareAndSwap(head, next) {
+			if v := head.v.Load(); v != nil {
+				elt = *v
+			}
+			q.tail.CompareAndSwap(head, nil)
+			q.sz.Add(-1)
+			return elt, true
+		}
 	}
-	q.Unlock()
-	return e, true
 }
 
 // After a pop(), the slice can be recycled for the next push() when
@@ -169,26 +167,19 @@ func (q *ipQueue[T]) recycle(elts *[]T) {
 	if elts == nil || *elts == nil {
 		return
 	}
-	// Update the in progress count.
-	if len(*elts) > 0 {
-		if atomic.AddInt64(&q.inprogress, int64(-(len(*elts)))) < 0 {
-			atomic.StoreInt64(&q.inprogress, 0)
-		}
-	}
+	c := int64(len(*elts))
+	q.inprogress.Add(-c)
 	// We also don't want to recycle huge slices, so check against the max.
 	// q.mrs is normally immutable but can be changed, in a safe way, in some tests.
 	if cap(*elts) > q.mrs {
 		return
 	}
-	q.resetAndReturnToPool(elts)
+	q.pool.Put(elts)
 }
 
 // Returns the current length of the queue.
 func (q *ipQueue[T]) len() int {
-	q.Lock()
-	l := len(q.elts) - q.pos
-	q.Unlock()
-	return l
+	return int(q.sz.Load())
 }
 
 // Empty the queue and consumes the notification signal if present.
@@ -199,18 +190,19 @@ func (q *ipQueue[T]) drain() {
 	if q == nil {
 		return
 	}
-	q.Lock()
-	if q.elts != nil {
-		q.resetAndReturnToPool(&q.elts)
-		q.elts, q.pos = nil, 0
+	for {
+		if head := q.head.Load(); q.head.CompareAndSwap(head, nil) {
+			q.tail.Store(nil)
+			q.sz.Store(0)
+			// Consume the signal if it was present to reduce the chance of a reader
+			// routine to be think that there is something in the queue...
+			select {
+			case <-q.ch:
+			default:
+			}
+			return
+		}
 	}
-	// Consume the signal if it was present to reduce the chance of a reader
-	// routine to be think that there is something in the queue...
-	select {
-	case <-q.ch:
-	default:
-	}
-	q.Unlock()
 }
 
 // Since the length of the queue goes to 0 after a pop(), it is good to
@@ -219,7 +211,7 @@ func (q *ipQueue[T]) drain() {
 // the pop() API. When the caller will call q.recycle(), this count will
 // be reduced by the size of the slice returned by pop().
 func (q *ipQueue[T]) inProgress() int64 {
-	return atomic.LoadInt64(&q.inprogress)
+	return q.inprogress.Load()
 }
 
 // Remove this queue from the server's map of ipQueues.
