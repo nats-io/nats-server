da64c7b5c93d1b4ab63b5b74e02c8c35dfdf20d4
Neil Twigg <neil@nats.io>
2024-06-17 16:20:22 +0100

    Bump to 2.10.17-RC.4
    Signed-off-by: Neil Twigg <neil@nats.io>

--- server/const.go
+++ server/const.go
@@ -41,7 +41,7 @@ var (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.10.17-RC.3"
+	VERSION = "2.10.17-RC.4"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
