54a1d72e466a245890fbf8afc1f3a986ac42d448
Alberto Ricart <alberto@synadia.com>
2024-06-17 14:10:16 -0500

    [FIX] added logic to enable users generated by a callout to be revoked - the callout is responsible to map the user ids for revocation purposes.

--- server/client.go
+++ server/client.go
@@ -261,6 +261,8 @@ type client struct {
 	msgb       [msgScratchSize]byte
 	last       time.Time
 	lastIn     time.Time
+	// this is a copy of the issued at (iat) field in the jwt
+	calloutIAT int64
 
 	headers bool
 
