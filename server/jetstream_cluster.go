c3157425f94c4717d1a7ec62862766b92d2cba3c
Derek Collison <derek@nats.io>
2024-06-27 12:12:50 -0700

    Not needed here and causes potential datarace with consumer info
    Signed-off-by: Derek Collison <derek@nats.io>

--- server/jetstream_cluster.go
+++ server/jetstream_cluster.go
@@ -4238,7 +4238,6 @@ func (js *jetStream) processConsumerRemoval(ca *consumerAssignment) {
 			if ca.Group != nil && oca.Group != nil && ca.Group.Name == oca.Group.Name {
 				needDelete = true
 				oca.deleted = true
-				oca.Group.node = nil
 				delete(sa.consumers, ca.Name)
 			}
 		}
