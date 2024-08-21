e12921459d6226dd7566ef5f672a8630f28e69ec
Waldemar Quevedo <wally@nats.io>
2024-08-21 08:12:41 -0700

    Bump to 2.10.19-RC.4+syn2
    Signed-off-by: Waldemar Quevedo <wally@nats.io>

--- server/const.go
+++ server/const.go
@@ -55,7 +55,7 @@ func init() {
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.10.19-RC.4+syn"
+	VERSION = "2.10.19-RC.4+syn2"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
