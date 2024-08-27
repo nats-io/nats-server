afa6c97311ffe2b8c8b04f06f120a584a17500cc
Byron Ruth <byron@nats.io>
2024-08-27 16:54:52 -0400

    Release v2.10.19
    Signed-off-by: Byron Ruth <byron@nats.io>

--- server/const.go
+++ server/const.go
@@ -55,7 +55,7 @@ func init() {
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.10.19-RC.6"
+	VERSION = "2.10.19"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
