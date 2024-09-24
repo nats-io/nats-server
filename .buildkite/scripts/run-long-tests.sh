8de837745e4af2f3bf5ccbbcb57e204a13395573
Marco Primi <marco@mpri.me>
2024-09-24 10:54:02 -0700

    Merge run and publish steps

--- .buildkite/scripts/run-long-tests.sh
+++ .buildkite/scripts/run-long-tests.sh
@@ -8,6 +8,16 @@ go version
 which gotestsum
 gotestsum --version
 
+which curl
+curl --version
+
+BUILDKITE_ANALYTICS_TOKEN="${BK_ANALYTICS_TOKEN_long_running_tests}"
+if [ -z "${BUILDKITE_ANALYTICS_TOKEN}" ];
+then
+  echo "Analytics token not set"
+  exit 1
+fi
+
 rm -f "${JUNIT_REPORT_PATH}"
 
 echo "🚀 Launching tests..."
@@ -22,7 +32,8 @@ if gotestsum \
   -v \
   -count=1 -vet=off -shuffle=on -p=1 \
   -timeout=60m \
-; then
+;
+then
   echo "All tests passed";
 else
   echo "Some tests failed";
@@ -32,3 +43,19 @@ if [[ ! -f "${JUNIT_REPORT_PATH}" ]]; then
   echo "Test report file not found: ${JUNIT_REPORT_PATH}"
   exit 1
 fi
+
+curl \
+  -X POST \
+  --fail-with-body \
+  -H "Authorization: Token token=\"$BUILDKITE_ANALYTICS_TOKEN\"" \
+  -F "data=@${JUNIT_REPORT_PATH}" \
+  -F "format=junit" \
+  -F "run_env[CI]=buildkite" \
+  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
+  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
+  -F "run_env[branch]=$BUILDKITE_BRANCH" \
+  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
+  -F "run_env[message]=$BUILDKITE_MESSAGE" \
+  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+  https://analytics-api.buildkite.com/v1/uploads
