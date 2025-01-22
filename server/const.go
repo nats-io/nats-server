fb122e0c7a6598ec7d65d553da55bdb11d309444
Neil Twigg <neil@nats.io>
2025-01-22 14:32:05 +0000

    Bump to 2.10.25-RC.3
    Signed-off-by: Neil Twigg <neil@nats.io>

--- server/const.go
+++ server/const.go
@@ -55,7 +55,7 @@ func init() {
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.10.25-RC.2"
+	VERSION = "2.10.25-RC.3"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
