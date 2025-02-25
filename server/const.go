50ee75cd6b63921d9f84053a9007b170b10cbeb6
Neil Twigg <neil@nats.io>
2025-02-25 13:41:44 +0000

    Release v2.10.26-RC.7
    Signed-off-by: Neil Twigg <neil@nats.io>

--- server/const.go
+++ server/const.go
@@ -55,7 +55,7 @@ func init() {
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.10.26-RC.6"
+	VERSION = "2.10.26-RC.7"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
