e3f9bed3f7b2c1a915f05ad7d6ec178d736e8155
Maurice van Veen <github@mauricevanveen.com>
2025-03-11 16:17:47 +0100

    De-flake TestJetStreamClusterMirrorAndSourceExpiration
    Signed-off-by: Maurice van Veen <github@mauricevanveen.com>

--- server/jetstream_cluster_2_test.go
+++ server/jetstream_cluster_2_test.go
@@ -1504,7 +1504,7 @@ func TestJetStreamClusterMirrorAndSourceExpiration(t *testing.T) {
 	sendBatch(100)
 	// Need to check both in parallel.
 	scheck, mcheck := uint64(0), uint64(0)
-	checkFor(t, 20*time.Second, 500*time.Millisecond, func() error {
+	checkFor(t, 20*time.Second, 100*time.Millisecond, func() error {
 		if scheck != 100 {
 			if si, _ := js.StreamInfo("S"); si != nil {
 				scheck = si.State.Msgs
