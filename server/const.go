c1105c899a3429288289c1603ade457d48f31356
Neil Twigg <neil@nats.io>
2025-03-19 18:01:32 +0000

    Bump `main` version to v2.11.1-dev
    [ci skip]
    
    Signed-off-by: Neil Twigg <neil@nats.io>

--- server/const.go
+++ server/const.go
@@ -58,7 +58,7 @@ func init() {
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.11.0"
+	VERSION = "2.11.1-dev"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
