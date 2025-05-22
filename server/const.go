4c2fc7f07d973a71d3bf14e30442120272e74931
Neil Twigg <neil@nats.io>
2025-05-22 14:21:11 +0100

    Release v2.11.4
    Signed-off-by: Neil Twigg <neil@nats.io>

--- server/const.go
+++ server/const.go
@@ -58,7 +58,7 @@ func init() {
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.11.4-RC.3"
+	VERSION = "2.11.4"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
