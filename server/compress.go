05a1ceedab5d7a2ba0203d6cea13e22450d1e7ec
aryehklein-rise <aryeh.klein@risecodes.com>
2025-07-12 22:47:25 +0300

    add sync.Pool for compression writers.

---
+++ server/compress.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"compress/gzip"
+	"io"
+	"sync"
+
+	"github.com/klauspost/compress/s2"
+)
+
+var Gzip gzipPool
+var Snappy snappyPool
+
+type gzipPool struct {
+	writers sync.Pool
+}
+
+type snappyPool struct {
+	writers sync.Pool
+}
+
+func (pool *gzipPool) GetWriter(dst io.Writer) (writer *gzip.Writer) {
+	if w := pool.writers.Get(); w != nil {
+		writer = w.(*gzip.Writer)
+		writer.Reset(dst)
+	} else {
+		writer = gzip.NewWriter(dst)
+	}
+
+	return writer
+}
+
+func (pool *gzipPool) PutWriter(writer *gzip.Writer) {
+	writer.Close()
+	pool.writers.Put(writer)
+}
+
+func (pool *snappyPool) GetWriter(dst io.Writer) (writer *s2.Writer) {
+	if w := pool.writers.Get(); w != nil {
+		writer = w.(*s2.Writer)
+		writer.Reset(dst)
+	} else {
+		writer = s2.NewWriter(dst, s2.WriterSnappyCompat())
+	}
+
+	return writer
+}
+
+func (pool *snappyPool) PutWriter(writer *s2.Writer) {
+	writer.Close()
+	pool.writers.Put(writer)
+}
