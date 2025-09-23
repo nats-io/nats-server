a56078ee5acb0a8c85998825022ee50fbe693af2
Neil Twigg <neil@nats.io>
2025-09-23 10:06:52 +0100

    Bump to v2.14.0-dev
    Signed-off-by: Neil Twigg <neil@nats.io>
    skip-checks: true

--- server/const.go
+++ server/const.go
@@ -66,7 +66,7 @@ func init() {
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "2.12.0"
+	VERSION = "2.14.0-dev"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
