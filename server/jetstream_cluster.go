a78652cde3baf735fb631d9689af4f1b9cdc8fd4
Alex Bozhenko <alexbozhenko@gmail.com>
2025-10-27 18:58:08 -0700

    add warning about legacy snapshot
    Signed-off-by: Alex Bozhenko <alexbozhenko@gmail.com>

--- server/jetstream_cluster.go
+++ server/jetstream_cluster.go
@@ -8775,6 +8775,7 @@ func (mset *stream) stateSnapshotLocked() []byte {
 		Deleted:  state.Deleted,
 	}
 	b, _ := json.Marshal(snap)
+	mset.srv.RateLimitWarnf("Stream %q: Using legacy JSON snapshot format. Snapshot length: %d bytes", mset.cfg.Name, len(b))
 	return b
 }
 
