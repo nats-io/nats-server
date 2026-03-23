7030346885d6694225c02399acc0b104d325cc06
Alex Bozhenko <alexbozhenko@gmail.com>
2026-03-23 14:38:01 +0300

    add total stream/consumer counts to meta snapshot stats

--- server/jetstream_cluster.go
+++ server/jetstream_cluster.go
@@ -80,6 +80,8 @@ type jetStreamCluster struct {
 	// Track last meta snapshot time and duration for monitoring.
 	lastMetaSnapTime     int64 // Unix nanoseconds
 	lastMetaSnapDuration int64 // Duration in nanoseconds
+	totalStreamCount     int64
+	totalConsumerCount   int64
 }
 
 // Used to track inflight stream create/update/delete requests that have been proposed but not yet applied.
@@ -2145,6 +2147,9 @@ func (js *jetStream) encodeMetaSnapshot(streams map[string]map[string]*streamAss
 	if cc := js.cluster; cc != nil {
 		atomic.StoreInt64(&cc.lastMetaSnapTime, start.UnixNano())
 		atomic.StoreInt64(&cc.lastMetaSnapDuration, int64(took))
+		atomic.StoreInt64(&cc.totalStreamCount, int64(nsa))
+		atomic.StoreInt64(&cc.totalConsumerCount, int64(nca))
+
 	}
 
 	return snap, nsa, nca, nil
