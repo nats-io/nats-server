929473bad48c125cf94fc7f35d3ab8b5aa06ddb9
Neil Twigg <neil@nats.io>
2026-04-07 13:02:43 +0100

    Ensure `LoadPrevMsgMulti` returns skip in `LoadPrevMsg` handoff, add test
    Signed-off-by: Neil Twigg <neil@nats.io>

--- server/filestore.go
+++ server/filestore.go
@@ -9146,8 +9146,15 @@ func (fs *fileStore) LoadPrevMsgMulti(sl *gsl.SimpleSublist, start uint64, smp *
 	}
 
 	if sl == nil || sl.MatchesFullWildcard() {
-		sm, err = fs.LoadPrevMsg(start, smp)
-		return
+		if sm, err = fs.LoadPrevMsg(start, smp); err == nil {
+			return sm, sm.seq, nil
+		}
+		if err == ErrStoreEOF {
+			fs.mu.RLock()
+			defer fs.mu.RUnlock()
+			return nil, fs.state.FirstSeq, err
+		}
+		return nil, 0, err
 	}
 	fs.mu.RLock()
 	defer fs.mu.RUnlock()
