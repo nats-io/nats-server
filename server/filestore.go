c9f1f7be19fb92645cde0dd0ebf0ac8fb0f83abc
Neil Twigg <neil@nats.io>
2026-04-07 12:35:06 +0100

    Load next/prev message will use non-multi path with full wildcard interest
    Signed-off-by: Neil Twigg <neil@nats.io>

--- server/filestore.go
+++ server/filestore.go
@@ -8366,7 +8366,7 @@ func (fs *fileStore) LoadNextMsgMulti(sl *gsl.SimpleSublist, start uint64, smp *
 	if fs.isClosed() {
 		return nil, 0, ErrStoreClosed
 	}
-	if sl == nil {
+	if sl == nil || sl.MatchesFullWildcard() {
 		return fs.LoadNextMsg(_EMPTY_, false, start, smp)
 	}
 	fs.mu.RLock()
@@ -8575,7 +8575,7 @@ func (fs *fileStore) LoadPrevMsgMulti(sl *gsl.SimpleSublist, start uint64, smp *
 		return nil, 0, ErrStoreClosed
 	}
 
-	if sl == nil {
+	if sl == nil || sl.MatchesFullWildcard() {
 		sm, err = fs.LoadPrevMsg(start, smp)
 		return
 	}
