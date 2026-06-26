8b2f640718e588440d8b230cafeca275abe0af27
Maurice van Veen <github@mauricevanveen.com>
2026-06-26 17:41:42 +0200

    (2.15) [IMPROVED] Set defaults to JetStream system-wide asset limits
    Signed-off-by: Maurice van Veen <github@mauricevanveen.com>

--- server/opts.go
+++ server/opts.go
@@ -6195,6 +6195,12 @@ func setBaselineOptions(opts *Options) {
 	if opts.JetStreamConcurrentIOs <= 0 {
 		opts.JetStreamConcurrentIOs = defaultConcurrentIOs
 	}
+	if opts.JetStreamLimits.MaxStreamsTotal == 0 {
+		opts.JetStreamLimits.MaxStreamsTotal = JSDefaultMaxStreamsTotal
+	}
+	if opts.JetStreamLimits.MaxConsumersTotal == 0 {
+		opts.JetStreamLimits.MaxConsumersTotal = JSDefaultMaxConsumersTotal
+	}
 }
 
 func getDefaultAuthTimeout(tls *tls.Config, tlsTimeout float64) float64 {
