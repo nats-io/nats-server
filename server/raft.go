0ad1d944f78b0c70d8725c9e16054a954b2b6b88
Neil Twigg <neil@nats.io>
2026-09-02 14:39:00 +0100

    NRG: Don't campaign if insufficient vote subject interest
    Signed-off-by: Neil Twigg <neil@nats.io>

--- server/raft.go
+++ server/raft.go
@@ -5917,6 +5917,20 @@ func (n *raft) switchToCandidate() {
 		return
 	}
 
+	// If not enough other nodes will hear our campaign, don't bother initiating it.
+	// It is possible that the partition will end and we will hear the current leader
+	// again and continue participating in the current term without another election.
+	// A single-node group can elect itself without any peer interest, which is
+	// needed to expand a group.
+	// NOTE: The metalayer in a supercluster always has a gateway connection and so
+	// this check does nothing for the _meta_ group in that case, this is known.
+	if n.qn > 1 && !n.t.SufficientInterest(n.vsubj, n.qn) {
+		n.debug("Not switching to candidate, not enough online peers to vote")
+		n.updateLeader(noLeader)
+		n.resetElect(minElectionTimeout)
+		return
+	}
+
 	if n.State() != Candidate {
 		n.debug("Switching to candidate")
 	} else {
