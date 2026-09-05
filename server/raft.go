d11500f11174a03eb924d5a687ef6ce40ca9312e
Ayush Kumar <ayush23162@iiitd.ac.in>
2026-09-05 20:35:46 +0530

    [FIXED] Remove stale JetStream account usage after peer removal
    Signed-off-by: Ayush Kumar <ayush23162@iiitd.ac.in>

--- server/raft.go
+++ server/raft.go
@@ -65,6 +65,7 @@ type RaftNode interface {
 	ID() string
 	Group() string
 	Peers() []*Peer
+	PeerNames() []string
 	ProposeKnownPeers(knownPeers []string)
 	UpdateKnownPeers(knownPeers []string)
 	ProposeAddPeer(peer string) error
@@ -4980,6 +4981,12 @@ func decodePeerState(buf []byte) (*peerState, error) {
 		ps.domainExt = extensionState(le.Uint16(buf[ri:]))
 	}
 	return ps, nil
+}
+
+func (n *raft) PeerNames() []string {
+	n.RLock()
+	defer n.RUnlock()
+	return n.peerNames()
 }
 
 // Lock should be held.
