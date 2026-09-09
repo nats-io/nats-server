95fe8d80be5a497024dd4a8ecf272ec2ebbb1c8e
Maurice van Veen <github@mauricevanveen.com>
2026-09-09 15:36:58 +0200

    (2.15) [IMPROVED] Clarify PeerInfo Pending and Active field comments
    Signed-off-by: Maurice van Veen <github@mauricevanveen.com>

--- server/stream.go
+++ server/stream.go
@@ -461,10 +461,10 @@ type PeerInfo struct {
 	Name    string        `json:"name"`              // Name is the unique name for the peer
 	Current bool          `json:"current"`           // Current indicates if it was seen recently and fully caught up
 	Offline bool          `json:"offline,omitempty"` // Offline indicates if it has not been seen recently
-	Active  time.Duration `json:"active"`            // Active is the timestamp it was last active
+	Active  time.Duration `json:"active"`            // Active is the nanoseconds since this peer was last seen
 	Lag     uint64        `json:"lag,omitempty"`     // Lag is how many operations behind it is
 	Peer    string        `json:"peer"`              // Peer is the unique ID for the peer
-	Pending bool          `json:"pending,omitempty"` // Pending indicates the peer is part of the assignment, but is not a peer of the Raft group (yet)
+	Pending bool          `json:"pending,omitempty"` // Pending indicates the peer is part of the assignment, but is not a peer of the Raft group yet or is being removed.
 	// For migrations.
 	cluster string
 }
