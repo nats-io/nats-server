118d926533c87de90aacb9557103b442b6b29028
Asritha Bodepudi <asrithabodepudi@gmail.com>
2026-09-18 11:48:49 -0400

    Preserve reply grants during inherited permission refresh
    Signed-off-by: Asritha Bodepudi <asrithabodepudi@gmail.com>

--- server/client_test.go
+++ server/client_test.go
@@ -1198,6 +1198,26 @@ func TestClientSetPermissionsClearsStaleMsgDenyState(t *testing.T) {
 	require_True(t, c.mperms == nil)
 }
 
+func TestClientUpdateDefaultPermissionsPreservesReplyGrants(t *testing.T) {
+	response := &ResponsePermission{MaxMsgs: 1, Expires: time.Second}
+	permissions := &Permissions{
+		Subscribe: &SubjectPermission{Allow: []string{"service"}},
+		Response:  response,
+	}
+	c := &client{user: &NkeyUser{Permissions: permissions, defaultPerms: true}}
+	c.setPermissions(permissions)
+	c.replies["reply.pending"] = &resp{t: time.Now()}
+
+	updated := permissions.clone()
+	updated.Subscribe.Allow = append(updated.Subscribe.Allow, "extra")
+	require_True(t, c.updateDefaultPermissions(updated))
+	require_NotNil(t, c.replies["reply.pending"])
+
+	updated.Response = nil
+	require_True(t, c.updateDefaultPermissions(updated))
+	require_Len(t, len(c.replies), 0)
+}
+
 func TestClientSetPermissionsPublishDenyQueueQualifierFailsClosed(t *testing.T) {
 	c := &client{srv: New(&Options{})}
 	c.setPermissions(&Permissions{
