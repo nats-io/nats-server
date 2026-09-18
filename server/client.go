47c486014995f2a0698cec607920703ae72a658f
Daniele Sciascia <daniele@nats.io>
2026-09-18 14:16:35 +0200

    [FIXED] Queue subscription deny check uses original subject
    Use the mapped delivery subject when evaluating subscription
    deny permissions for queue subscribers. This makes queue
    delivery checks consistent with regular subscriptions.
    
    Signed-off-by: Daniele Sciascia <daniele@nats.io>

--- server/client.go
+++ server/client.go
@@ -5646,7 +5646,7 @@ func (c *client) processMsgResults(acc *Account, r *SublistResult, msg, deliver,
 			var delivered bool
 			if !skipDelivery {
 				mh := c.msgHeader(dsubj, creply, sub)
-				delivered = c.deliverMsg(prodIsMQTT, sub, acc, subject, creply, mh, msg, rplyHasGWPrefix)
+				delivered = c.deliverMsg(prodIsMQTT, sub, acc, dsubj, creply, mh, msg, rplyHasGWPrefix)
 				if restorePaTrace {
 					c.pa.trace = mt
 				}
