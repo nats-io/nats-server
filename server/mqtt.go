b46f0e6b6b3da24b21aeaa09ef4175ef59ed8971
Lev Brouk <levbrouk@gmail.com>
2026-09-19 08:07:58 -0700

    [IMPROVED] MQTT: room for the CRLF in messages built for a JS API store
    sendJSAPIrequests appends the CRLF to a store without a message id (a
    QoS1 PUBLISH, a QoS2 hold, a retained message). Sized exactly, the
    message was reallocated and copied for it; with LEN_CR_LF spare
    capacity the append is in place.
    
    Signed-off-by: Lev Brouk <levbrouk@gmail.com>

--- server/mqtt.go
+++ server/mqtt.go
@@ -3036,7 +3036,8 @@ func mqttEncodeRetainedMessage(rm *mqttRetainedMsg) (natsMsg []byte, headerLen i
 		l += len(rm.Msg)
 	}
 
-	buf := bytes.NewBuffer(make([]byte, 0, l))
+	// +LEN_CR_LF: sendJSAPIrequests appends the CRLF in place.
+	buf := bytes.NewBuffer(make([]byte, 0, l+LEN_CR_LF))
 
 	buf.WriteString(hdrLine)
 
@@ -4433,7 +4434,8 @@ func mqttComputeNatsMsgSize(pp *mqttPublish, encodePP bool) int {
 func mqttNewDeliverableMessage(pp *mqttPublish, encodePP bool) (natsMsg []byte, headerLen int) {
 	size := mqttComputeNatsMsgSize(pp, encodePP)
 
-	buf := bytes.NewBuffer(make([]byte, 0, size))
+	// +LEN_CR_LF: sendJSAPIrequests appends the CRLF in place.
+	buf := bytes.NewBuffer(make([]byte, 0, size+LEN_CR_LF))
 
 	qos := mqttGetQoS(pp.flags)
 
